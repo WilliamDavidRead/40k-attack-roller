f28c744785874a429df94603f7beb86a7eac1d76
WIll <williamdavidread@gmail.com>
2022-06-23 22:45:20 +0100

    Added first draft attack rerolls

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import random
 
 
-def attackroll(bskill, nshots, strength, toughness):
+def attackroll(bskill, nshots, strength, toughness, reroll):
     hitnum = 0
     hitcount = 0
     woundnum = 0
     shots = []
     wounds = []
+    rerollbool = False
+    if reroll == 'yes' or reroll == 'y' or reroll == 'Yes':
+        rerollbool = True
+        reroll = int(input('What numbers do you reroll? '))
     for shot in range(nshots):
         myroll = random.randint(1, 6)
         shots.append(myroll)
+        if myroll < bskill and int(myroll) <= reroll and rerollbool:
+            myroll = random.randint(1, 6)
+            shots.append(myroll)
         if myroll >= bskill:
             hitnum += 1
             hitcount = hitnum
@@ -18,8 +25,8 @@ def attackroll(bskill, nshots, strength, toughness):
         woundsuccess = False
         mywound = random.randint(1, 6)
         wounds.append(mywound)
-        if (strength <= toughness/2 and mywound > 5) or ((strength > toughness / 2) and mywound > 4)\
-                or (strength == toughness and mywound > 3) or\
+        if (strength <= toughness / 2 and mywound > 5) or ((strength > toughness / 2) and mywound > 4) \
+                or (strength == toughness and mywound > 3) or \
                 ((strength > toughness) and (strength < toughness * 2) and mywound > 2):
             woundsuccess = True
         if woundsuccess:
@@ -27,12 +34,13 @@ def attackroll(bskill, nshots, strength, toughness):
     print('Hit rolls: ', shots)
     print('Number of hits: ', hitnum)
     print('Wound rolls: ', wounds)
-    print('Number of wounds: ', woundnum)
+    print('Number of successful wound rolls: ', woundnum)
     return woundnum
 
 
 attackroll(bskill=int(input('What is your Weapon/Ballistic Skill? ')), nshots=int(input('How many shots? ')),
-         strength=int(input('What is your strength? ')), toughness=int(input('What is the target toughness? ')))
+           strength=int(input('What is your strength? ')), toughness=int(input('What is the target toughness? ')),
+           reroll=input('Do you have any rerolls? '))
 
 
 def savingthrow(savenum, ap, save, fnp):
@@ -62,12 +70,12 @@ def savingthrow(savenum, ap, save, fnp):
                 finalfails -= 1
     if fnptest:
         print('Saving throw rolls: ', saves)
-        print('Number of saves failed: ', failedsaves)
+        print('Number of failed saves: ', failedsaves)
         print('Feel no pain rolls: ', fnprolls)
-        print('Number of wounds taken: ', finalfails)
+        print('Number of failed feel no pain rolls: ', finalfails)
     else:
         print('Saving throw rolls: ', saves)
-        print('Number of wounds taken: ', finalfails)
+        print('Number of failed saves: ', finalfails)
 
 
 savingthrow(savenum=int(input('How many saving throws? ')), ap=int(input('What is the ap of the attacker? ')),
